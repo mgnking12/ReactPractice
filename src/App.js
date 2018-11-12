c538329633ece4ee1be9a7b28c4b22d5d88e0ee5
Megan King <mgnking12@gmail.com>
2018-11-12 16:20:10 -0600

    working with setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+ const list = [
+                  {
+                  title: 'React',
+                  url: 'https://reactjs.org/',
+                  author: 'Jordan Walke',
+                  num_comments: 3,
+                  points: 4,
+                  objectID: 0,
+                  },
+                  {
+                  title: 'Redux',
+                  url: 'https://redux.js.org/',
+                  author: 'Dan Abramov, Andrew Clark',
+                  num_comments: 2,
+                  points: 5,
+                  objectID: 1,
+                  },
+                  ];
+
+// 'class App' is declaring a component using React ES6 class component, after you have declared a component, you can use it as an element anywhere in your application by creating an instance of it ex: <App />
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id){
+  const notId = item => item.objectID !== id;
+  const updatedList = this.state.list.filter(notId);
+  this.setState({ list: updatedList });
+  }
+
   render() {
+   
     return (
+      // className is the same thing as 'class' in HTML but has to be used in place of it for JSX. All HTML attributes in JSX are going to follow the camelCase rule
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     
+          {this.state.list.map(item =>  //when using js es6 you can remove the parenthesis around the argument if you are only going to have one. 
+            // return ( /////////this is used when you have a "block body" we removed it because we had a "concise body" when using ES6 and in a concise body an implicit return is attached 
+              // React can identify modified items when the list changes with a key identifier ////// avoid using the index in an array as the idenifier, the index is not stable and you always need a stable key identifier 
+              <div key={item.objectID}>
+                <ul>
+                  <li className="title">
+                    <a href={item.url}>{item.title}</a>
+                  </li>
+                  <li>{item.author}</li>
+                  <li>{item.num_comments}</li>
+                  <li>{item.points}</li>
+                </ul>
+                <button 
+                onClick={() => this.onDismiss(item.objectID)} 
+                type="button">
+                Dismiss
+                </button>
+              </div>
+            )
+          // )
+          }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+//  you can use ReactDOM.render() multiple times across your application, but in a plain React application, you would only use it once tobootstrap the component tree. ////// This is always going to expect two arguments, the first, a components to render JSX or simple JSX itself, the second, where to place the React App in our HTML file. 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement, shows your changes in the browser without having to reloading the page //////// The most useful benefit of HMR is that you can keep the application state after the applicationreloads.
+if (module.hot) {
+module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
